b279d50b13ce9c625d16a827c8741e302c4f0b14
Yehonal <hw.2@hotmail.it>
2014-08-05 16:36:56 +0200

    fixed package names

--- MenuItem.java
+++ MenuItem.java
@@ -3,7 +3,7 @@
  * GNU General Public License version 3; see www.hyperweb2.com/terms/
  */
 
-package Hw2.Java.library.climenu;
+package hw2.java.library.climenu;
 
 import java.util.concurrent.Callable;
 
